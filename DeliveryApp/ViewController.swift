711f2c799c74515c4f6e172f4b808e04d329212f
João Lucas <joao.lucas.f.pereira@accenture.com>
2023-03-22 23:49:36 -0300

    First commit

---
+++ DeliveryApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  DeliveryApp
+//
+//  Created by joao.lucas.f.pereira on 22/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+    }
+}

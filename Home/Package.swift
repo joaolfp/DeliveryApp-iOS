9c5cf475ebb006bd86a011e9f7c5f80ca4062554
joaolfp <joaolucasfp2001@gmail.com>
2023-09-10 10:55:28 -0300

    Updated version NimbleSnapshots

--- Home/Package.swift
+++ Home/Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         .package(path: "./TestUtils"),
         .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "7.2.0")),
         .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "12.2.0")),
-        .package(url: "https://github.com/ashfurrow/Nimble-Snapshots", branch: "master"),
+        .package(url: "https://github.com/joaolfp/Nimble-Snapshots", branch: "1.0.0"),
         .package(url: "https://github.com/joaolfp/ViewState.git", .upToNextMajor(from: "1.3.3")),
         .package(url: "https://github.com/dogo/SketchKit", .upToNextMajor(from: "2.0.0")),
         .package(url: "https://github.com/heroesofcode/MagicImages", .upToNextMajor(from: "1.0.0"))

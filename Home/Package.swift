c7acc2454404a6a46028f73f79c84f14f3275d03
joaolfp <joaolucasfp2001@gmail.com>
2023-10-03 21:32:20 -0300

    Update MagicImages to 1.3.0 (was 1.2.0)

--- Home/Package.swift
+++ Home/Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         .package(path: "./TestUtils"),
         .package(url: "https://github.com/joaolfp/ViewState.git", .upToNextMajor(from: "1.4.0")),
         .package(url: "https://github.com/dogo/SketchKit", .upToNextMajor(from: "2.0.0")),
-        .package(url: "https://github.com/heroesofcode/MagicImages", .upToNextMajor(from: "1.2.0")),
+        .package(url: "https://github.com/heroesofcode/MagicImages", .upToNextMajor(from: "1.3.0")),
         .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.13.0")
     ],
     targets: [

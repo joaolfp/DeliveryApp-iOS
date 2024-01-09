204d023e7acdda7dde6db574dcb89e4f977dc176
joaolfp <joaolucasfp2001@gmail.com>
2024-01-08 23:51:03 -0300

    Update SnapshotTesting to 1.15.2 (was 1.15.1)

--- Home/Package.swift
+++ Home/Package.swift
@@ -26,7 +26,7 @@ let package = Package(
         .package(url: "https://github.com/joaolfp/ViewState.git", .upToNextMajor(from: "1.6.0")),
         .package(url: "https://github.com/dogo/SketchKit", .upToNextMajor(from: "2.0.0")),
         .package(url: "https://github.com/heroesofcode/MagicImages", .upToNextMajor(from: "1.4.0")),
-        .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.15.1")
+        .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.15.2")
     ],
     targets: [
         .target(
